99b293ec9b26954243944de3c617891a65de23fc
avalusa1 <avalusa1@toromail.csudh.edu>
2026-03-21 20:49:25 -0500

    🔥 CRITICAL: Remove ALL Loading component and LoadingProvider - render immediately

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import "./App.css";
-
 import MainContainer from "./components/MainContainer";
-import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
-  return (
-    <>
-      <LoadingProvider>
-        <MainContainer />
-      </LoadingProvider>
-    </>
-  );
+  return <MainContainer />;
 };
 
 export default App;
